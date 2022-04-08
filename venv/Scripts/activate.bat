a9b2bbde07b2443b25d08181e62a827d33a37f88
Heuptling <Heuptling@yandex.ru>
2022-04-08 16:55:28 +0300

    Финальная версия ноутбука. Сделано всё, что возможно.

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\zetet\Jupyter\Projects\auto_pricing\venv
+set VIRTUAL_ENV=C:\Users\Anton\SFF\Auto\Auto_price\auto_pricing\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
